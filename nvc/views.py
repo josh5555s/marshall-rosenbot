773bf673bbdff6332cd49188ef8099e396117d5e
BuildTools <unconfigured@null.spigotmc.org>
2022-04-21 11:19:41 -0700

    basic nvc pages

--- nvc/views.py
+++ nvc/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+from .models import Feeling, FeelingGroup, Need, NeedGroup, Poem
 
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Feeling
+    template_name = 'nvc/index.html'
+    context_object_name = 'home'
+
+            
+
+
+class DetailView(generic.DetailView):
+    model = Feeling
+    template_name = 'nvc/detail.html'
+
+    # def get_queryset(self):
+    #     """
+    #     Excludes any questions that aren't published yet.
+    #     """
+    #     return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
+
+#     def get_queryset(self):
+#         """
+#         Excludes any results for questions that aren't published yet.
+#         """
+#         return Question.objects.filter(pub_date__lte=timezone.now())
+
+
--- tutorials/urls.py
+++ tutorials/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('nvc/', include('nvc.urls')),
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 ]
